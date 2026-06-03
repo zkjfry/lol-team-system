214540dd778cdec775ca411c18234d68345c76b4
zkjfry <jeffreychen0826@gmail.com>
2026-06-03 17:04:01 +0800

    update cors config

--- backend/src/main/java/com/example/lolteambackend/config/CorsConfig.java
+++ backend/src/main/java/com/example/lolteambackend/config/CorsConfig.java
@@ -19,7 +19,8 @@ public class CorsConfig {
                                 "http://localhost:3000",
                                 "http://localhost:5173",
                                 "http://127.0.0.1:5500",
-                                "http://localhost:5500"
+                                "http://localhost:5500",
+                                "https://zkjfry.github.io"
                         )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
